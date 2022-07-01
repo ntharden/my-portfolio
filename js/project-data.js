38201546e33966a9c794516f33132835b9aaed4b
nathanth <nathan.hardenburg@icloud.com>
2022-07-01 09:17:15 -0500

    readme images

--- js/project-data.js
+++ js/project-data.js
@@ -5,6 +5,13 @@ let projectData = [
     image: "/assets/ttt.png",
     github: "https://github.com/ntharden/ttt-weekend",
     description: "Battle between X's and O's"
+  },
+  {
+    title: "Restaurant Tracker",
+    deployment: "https://restaurant-tracker-ga.herokuapp.com/restaurants",
+    image: "/assets/rest-tracker.png",
+    github: "https://github.com/ntharden/restaurant-tracker",
+    description: "Track your favorite restaurants"
   }
 ]
 
