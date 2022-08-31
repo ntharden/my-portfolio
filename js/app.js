3020eb6241fc8f6d33878328523adb9c954b29d9
nathanth <nathan.hardenburg@icloud.com>
2022-08-31 17:21:08 -0500

    adds logos to overlay

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
 import { projectData } from "./project-data.js";
 
-
+const homeBtn = document.getElementById('home-btn')
+const aboutBtn = document.getElementById('about-btn')
+const workBtn = document.getElementById('work-btn')
+const skillsBtn = document.getElementById('skills-btn')
+const homeSection = document.querySelector('.home')
+const aboutSection = document.querySelector('.about')
+const workSection = document.querySelector('.work')
+const skillsSection = document.querySelector('.skills')
 const cardContainer = document.getElementById("card-container")
+const navBar = document.querySelector('.navbar')
+const mediaQuery = window.matchMedia('(max-width: 768px)')
 
 let project = {
   title: "Tic Tac Toe",
@@ -10,8 +19,6 @@ let project = {
   github: "https://github.com/ntharden/ttt-weekend",
   description: "Battle between X's and O's"
 }
-
-console.log(project)
 
 let projectMarkup = projectData.map(project =>
   `
@@ -30,3 +37,22 @@ let projectMarkup = projectData.map(project =>
 ).join("")
 
 cardContainer.innerHTML = projectMarkup
+
+homeBtn.addEventListener('click', function() {
+  homeSection.scrollIntoView({behavior: 'smooth'})
+  });
+aboutBtn.addEventListener('click', function() {
+  aboutSection.scrollIntoView({behavior: 'smooth'})
+  });
+workBtn.addEventListener('click', function() {
+  workSection.scrollIntoView({behavior: 'smooth'})
+  });
+skillsBtn.addEventListener('click', function() {
+  skillsSection.scrollIntoView({behavior: 'smooth'})
+  });
+window.addEventListener('scroll', function() {
+  if (!mediaQuery.matches) {
+      const windowPosition = window.scrollY > 1
+      navBar.classList.toggle('scrolling-active', windowPosition)
+  } else return
+  })
